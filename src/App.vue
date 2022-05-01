113527b00b9b194310203af86eb1f2124057e9a8
senurajayasinghe.se@gmail.com <senurajayasinghe.se@gmail.com>
2022-05-02 03:37:47 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+<Header />
+<div class="container">
+  <RouterView />
+  </div>
+</template>
+
+<script>
+ import Header from './components/Header.vue';
+ 
+
+
+
+
+
+export default {
+  components: {
+    Header,
+  
+
+  },
+}
+</script>
+
